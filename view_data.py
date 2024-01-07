cbc9652ceb4281ddae2dba12f1cdf607415339d0
Scott Chellew <scott.chellew@outlook.com>
2024-01-07 15:35:41 +0000

    Creates view_data

--- view_data.py
+++ view_data.py
@@ -1,4 +1,21 @@
 import pandas as pd
 
 dataframe = pd.read_csv('loan_payments.csv')
-dataframe.head()
+pd.set_option('display.max_columns', None)
+dataframe.tail()
+
+#datatypes = dataframe.dtypes
+#print(datatypes)
+#loan_type = dataframe['loan_status'].unique()
+#print(loan_type)
+#sub = dataframe['sub_grade'].unique()
+#sub.sort()
+#print(sub)
+#employment = dataframe['employment_length'].unique()
+#print(employment)
+#verify = dataframe['verification_status'].unique()
+#print(verify)
+#app = dataframe['application_type'].unique()
+#print(app)
+#policy = dataframe['policy_code'].unique()
+#print(policy)
